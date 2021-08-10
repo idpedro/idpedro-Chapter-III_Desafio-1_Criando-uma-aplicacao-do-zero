7e70cd40e2b5d79d1cca69fea9d27145348f7224
Pedro Ferreira <pedorferreira@ferreirasolucoes.site>
2021-08-09 22:46:47 -0300

    Create Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+import commonStyles from '../../styles/common.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className={`${commonStyles.container} ${styles.container}`}>
+      <a href="/">
+        <img src="/Logo.svg" alt="logo" />
+      </a>
+    </div>
+  );
 }
