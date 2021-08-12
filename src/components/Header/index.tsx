d27625e44526024500bcd64d99cc907d56f8e13f
Pedro Ferreira <pedorferreira@ferreirasolucoes.site>
2021-08-12 15:13:25 -0300

    use Link componet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 import commonStyles from '../../styles/common.module.scss';
+import {} from 'next';
 
 export default function Header(): JSX.Element {
   return (
     <div className={`${commonStyles.container} ${styles.container}`}>
-      <a href="/">
+      <Link href="/">
         <img src="/Logo.svg" alt="logo" />
-      </a>
+      </Link>
     </div>
   );
 }
